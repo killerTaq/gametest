5949aa1f9464e1e0e0cfaf76dad2fea5da319dcc
ankita <ankitakumariverma30@gmail.com>
2019-05-03 19:28:12 +0530

    try

---
+++ GIT_Test_Project/Assets/Scenes/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	void Update ()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical"));	
+	}
+}
