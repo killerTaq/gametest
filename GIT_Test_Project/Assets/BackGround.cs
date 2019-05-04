262a0a7d1d4806a458e25b7c653d6c1022e6929f
ankita <ankitakumariverma30@gmail.com>
2019-05-04 12:48:40 +0530

    test

---
+++ GIT_Test_Project/Assets/BackGround.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour {
+
+    Material material;
+    Vector2 offset;
+    public float xVelocity, yVelocity;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+
+    }
+    void Start () {
+		
+	}
+
+	void Update () {
+        offset = new Vector2(xVelocity, yVelocity);
+        material.mainTextureOffset += offset * Time.deltaTime;
+        Vector2 temp = new Vector2(0, 0);
+             
+        if (material.mainTextureOffset.x == 0)
+        {
+            material.mainTextureOffset = temp;
+        }
+
+	}
+}
